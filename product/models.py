c4f3c198eb45db30861b5c0876c779b2f2c880a0
Egill Smári Snorrasons <egill98@hotmail.com>
2020-05-04 18:10:23 +0000

    models and database connection

--- product/models.py
+++ product/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-''' Endilega finna fleiri attributes'''
+''' Endilega finna fleiri attributess'''
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
