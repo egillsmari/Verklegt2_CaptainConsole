e3441493324933e5555493a8f855fc7a1f774ede
Andri Pétursson <afannar@gmail.com>
2020-05-05 15:09:15 +0000

    products have appeared

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from product.models import Product
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'homepage/index.html')
+    context = {'products': Product.objects.all().order_by('category')}
+    return render(request, 'homepage/index.html', context)
