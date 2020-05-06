1a8c60c926ad8678856e9755f622f761eea48966
Andri Pétursson <afannar@gmail.com>
2020-05-06 20:01:54 +0000

    mega

--- homepage/views.py
+++ homepage/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from product.models import Product
-from context.contextBuilder import platformsContext
+from context.contextBuilder import manufacturerContext
 
 
 # Create your views here.
 def index(request):
-    context = platformsContext()
+    context = manufacturerContext()
     context['products'] = Product.objects.all().order_by('category_id')
     return render(request, 'homepage/index.html', context)
