7fd29a32de16fef0909c30042cb9c343e136cead
Andri Pétursson <afannar@gmail.com>
2020-05-10 15:01:34 +0000

    virkar

--- product/urls.py
+++ product/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('<int:category>/<int:manufacturer>', views.index, name='product-index'),
-    path('<int:category>/<int:manufacturer>/<int:platform>', views.productFilter, name='product-productFilter'),
-    path('<int:category>/<int:manufacturer>/<str:sort>', views.productSort, name='product-productSort'),
+    path('<int:category>/<int:manufacturer>/filter/<int:platform>', views.productFilter, name='product-productFilter'),
+    path('<int:category>/<int:manufacturer>/sort/<int:sort>', views.productSort, name='product-productSort'),
     path('<int:category>/<int:manufacturer>/<int:range>', views.productRange, name='product-productRange')
 ]
