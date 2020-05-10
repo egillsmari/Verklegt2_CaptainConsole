f0c2dd8d433af27ec9133b7a685915ab79ddde05
halldorholgersson <halldor1995@gmail.com>
2020-05-10 19:18:30 +0000

    not valid page search

--- product/urls.py
+++ product/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('<int:category>/<int:manufacturer>', views.index, name='product-index'),
     path('<int:category>/<int:manufacturer>/<int:platform>', views.productFilter, name='product-productFilter'),
-    path('<int:category>/<int:manufacturer>/<str:sort>', views.productSort, name='product-productSort'),
+    path('<int:category>/<int:manufacturer>/<int:sort>', views.productSort, name='product-productSort'),
     path('<int:category>/<int:manufacturer>/<int:range>', views.productRange, name='product-productRange')
 ]
--- product/views.py
+++ product/views.py
@@ -3,6 +3,9 @@ from product.models import Product
 from product.models import Platform
 from context.contextBuilder import allContext, narrowContext
 
+import logging
+logger = logging.getLogger(__name__)
+
 # Create your views here.
 def index(request, category, manufacturer):
     if manufacturer == 0:
@@ -14,25 +17,31 @@ def index(request, category, manufacturer):
         context['products'] = Product.objects.filter(category_id=category, platform_id__in=plat).order_by('name')
     return render(request, 'product/index.html', context)
 
-def productSort(request, category, manufacturer, sort):
-    context = narrowContext(category, manufacturer)
-    if sort == 0:
-        context['products'] = Product.objects.filter(category_id=category, manufacturer_id=manufacturer).order_by('price')
-    elif sort == 1:
-        context['products'] = Product.objects.filter(category_id=category, manufacturer_id=manufacturer).order_by('-price')
-    elif sort == 2:
-        pass
-    elif sort == 3:
-        context['products'] = Product.objects.filter(category_id=category, manufacturer_id=manufacturer).order_by('releaseDate')
-    return render(request, 'product/index.html', context)
 
 def productFilter(request, category, manufacturer, platform):
     context = narrowContext(category, manufacturer)
-    context['products'] = Product.objects.filter(category_id=category, manufacturer_id=manufacturer, platform_id=platform).order_by('name')
+    context['products'] = Product.objects.filter(category_id=category, platform_id=platform).order_by('name')
     return render(request, 'product/index.html', context)
+
+
+def productSort(request, category, manufacturer, sort):
+    context = narrowContext(category, manufacturer)
+    plat = Platform.objects.filter(manufacturer_id=manufacturer).values_list('id', flat=True)
+
+    if sort == 0:
+        context['products'] = Product.objects.filter(category_id=category, platform_id__in=plat).order_by('price')
+    elif sort == 1:
+        context['products'] = Product.objects.filter(category_id=category, platform_id__in=plat).order_by('-price')
+    elif sort == 2:
+        pass
+    elif sort == 3:
+        context['products'] = Product.objects.filter(category_id=category, platform_id__in=plat).order_by('releaseDate')
+    return render(request, 'product/index.html', context)
+
 
 def productPlatform():
     pass
+
 
 def productRange():
     pass
