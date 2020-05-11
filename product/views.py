110af504d3a97ba2f9fec66f1554462a83da5ef6
halldorholgersson <halldor1995@gmail.com>
2020-05-11 18:25:47 +0000

    get andri new stuff

--- product/views.py
+++ product/views.py
@@ -29,38 +29,6 @@ def productFilter(request, category, manufacturer, platform):
     return render(request, 'product/index.html', context)
 
 
-
-def productSort(request, category, manufacturer, sort):
-    context = narrowContext(category, manufacturer)
-    plat = Platform.objects.filter(manufacturer_id=manufacturer).values_list('id', flat=True)
-
-def productSort(request, category, manufacturer, sort):
-    context = narrowContext(category, manufacturer)
-    plat = Platform.objects.filter(manufacturer_id=manufacturer).values_list('id', flat=True)
-
-    if sort == 0:
-        context['products'] = Product.objects.filter(category_id=category, platform_id__in=plat).order_by('price')
-    elif sort == 1:
-        context['products'] = Product.objects.filter(category_id=category, platform_id__in=plat).order_by('-price')
-    elif sort == 2:
-        pass
-    elif sort == 3:
-        context['products'] = Product.objects.filter(category_id=category, platform_id__in=plat).order_by('releaseDate')
-    return render(request, 'product/index.html', context)
-
-
-
-def productPlatform():
-    pass
-
-
-def productRange():
-    pass
-
-def productRange(request, category, manufacturer):
-    context = narrowContext(category, manufacturer)
-    plat = Platform.objects.filter(manufacturer_id=manufacturer).values_list('id', flat=True)
-
 def productSort(request, category, manufacturer, filter, sort):
     if manufacturer == 0:
         context = allContext(category, manufacturer)
