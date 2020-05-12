8bdf493f5f380446de74da02eb9ec6a12c865bd3
Egill Smári Snorrasons <egill98@hotmail.com>
2020-05-12 18:39:49 +0000

    lets go með checkout

--- checkout/models.py
+++ checkout/models.py
@@ -9,6 +9,7 @@ from django.contrib.auth.models import User
 class Order(models.Model):
     accountId = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     productId = models.ForeignKey(Product, on_delete=models.CASCADE)
+    shippingMethod = models.CharField(default='pickUp', max_length=10)
 
 class Sold(models.Model):
     orderId = models.ForeignKey(Order, on_delete=models.CASCADE)
